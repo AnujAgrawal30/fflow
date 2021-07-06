b8078152b5d3b42489ef72e051ca5cd7f4c6c75e
Domingo <ing.domingos.l@gmail.com>
2021-07-06 20:06:26 +0200

    Copy fix

--- clients_src/site/js/main.js
+++ clients_src/site/js/main.js
@@ -5,7 +5,7 @@ const Modal = require('../../app/js/modals/abstract');
 
 Modal.Alert(
     "info",
-    "Hi! and thank you for coming!. FFlow even tho fully functional, it is, for now, a fictional product created as a contender" +
+    "Hi! and thank you for coming!. FFlow even tho fully functional, it is, for now, a fictional product created as a contender " +
     "for the first prize of the international hackathon formula 0001 organized by Rapyd. " +
     "Any reference to Rapyd intellectual material is used only as an example, and all user information collected during the life of this demo " +
     "will be wipe out after the hackathon is over. For now, please enjoy! (BTW you need an invitation code to access 😎)",
