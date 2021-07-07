d42fccdf006f113cff7a105ef2e9d76d0ff38c97
Domingo <ing.domingos.l@gmail.com>
2021-07-07 16:57:04 +0200

    Better doc

--- common/blocks/network-incoming-webhook.js
+++ common/blocks/network-incoming-webhook.js
@@ -1,16 +1,21 @@
 const LiteGraph = require('../lib/litegraph');
 const AbstractBlock = require('./abstract-block');
 
-//import * as docs from "./network-incoming-webhook.md";
 
 const debounce = require('debounce');
 
 class NetworkIncomingWebhook extends AbstractBlock {
 
     static title = "Incoming Webhook";
-    //static desc = docs.html;
-    static desc = "TODO";
+    static desc = "Allows to receive a webhook at the indicated URL. " +
+        "See the configuration windows to setup Method, Content type and a Secret. The last one is " +
+        "used for signing the payload in order to keep your flows login safe. Add the header X-Signature-sha256 " +
+        "with a value \"sha256=HMAC('sha256', secret, bodyAsString, 'hex')\". <br /><br /> NodeJS example: <br /><br />" +
+        "<input style=\"width: 100%\" type='text' value='header[\"X-Signature-sha256\"] = \"sha256=\" + crypto.createHmac(\"sha256\", flow.incomingUserWebhook.secret).update(JSON.stringify(req.body)).digest(\"hex\")'/>";
+
     static menu = "Network/Incoming Webhook";
+
+
 
     constructor(props) {
 
